7ebca360bad8e2e5e52e809e6bfbfc66dc5bad93
Andreas Jensen <andreas-jensen@hotmail.dk>
2017-02-08 16:52:52 +0100

    Tilføjet klassen Fødevare

---
+++ OperationLifeChanger/Models/Fødevare.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OperationLifeChanger.Models
+{
+    public class Fødevare
+    {
+        public double Protein;
+
+        public double Fedt;
+
+        public double Kulhydrat; 
+    }
+}
